1342353038bb81427a7f79526cde0bf51d6ef69c
Karthikeyan Padaikathu <141762547+kartykp@users.noreply.github.com>
2024-07-09 15:07:41 -0700

    Bump the braces package version 3.0.2 to 3.0.3 (#188)
    * Bump the braces package version 3.0.2 to 3.0.3
    
    * lint fix

--- packages/verify-provenance/src/index.ts
+++ packages/verify-provenance/src/index.ts
@@ -1,6 +1,5 @@
 // TODO
-export const verifyProvenance = (
-  bundle: object,
-): boolean => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const verifyProvenance = (bundle: object): boolean => {
   return false;
 };
